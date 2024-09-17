1eea65ec157f4a7a5e3dbcb7088eb69f3a62bea0
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-09-17 09:06:09 -0300

    Create summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Dialog } from './components/dialog'
 import { CreateGoal } from './components/create-goal'
-import { EmptyGoals } from './components/empty-goals'
+import { Summary } from './components/summary'
+// import { EmptyGoals } from './components/empty-goals'
 
 export function App() {
   return (
     <Dialog>
-      <EmptyGoals />
+      {/* {<EmptyGoals />} */}
+      <Summary />
       <CreateGoal />
     </Dialog>
   )
