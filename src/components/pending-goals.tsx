561ee3376d76b653abba65d9ca5becf6a7f20fb0
FernandoJunior7 <fernando.junior@edu.unifil.br>
2024-09-18 12:19:37 -0300

    Finish all the style of the summary package
    Connected to the back-end and disabling the button when already completed
    the desired weekly frequency of the goal

---
+++ src/components/pending-goals.tsx
@@ -0,0 +1,30 @@
+import { Plus } from 'lucide-react'
+import { OutlineButton } from './outline-button'
+import { getPendingGoals } from '../http/get-pending-goals'
+import { useQuery } from '@tanstack/react-query'
+
+export function PendingGoals() {
+  const { data } = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals,
+    staleTime: 1000 * 60 * 15, // 15 minutos,
+  })
+
+  if (!data) return null
+
+  return (
+    <div className="flex flex-wrap gap-3">
+      {data.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
+    </div>
+  )
+}
